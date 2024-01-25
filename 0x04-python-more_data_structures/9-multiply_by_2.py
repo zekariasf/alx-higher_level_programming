158a612f3e50f177f805cdbda545777785bfbf47
zekariasf <zekarias.fikru@gmail.com>
2024-01-25 13:33:46 +0000

    0x01

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
     for k, v in new_dict.items():
-        new_dict[k] = value * 2
+        new_dict[k] = v * 2
     return new_dict
